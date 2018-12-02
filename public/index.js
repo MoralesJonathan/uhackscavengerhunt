6eaf773d3759d65a5aa8a104e2aef37edd7b662d
Fulton Garcia <fultongarcia@gmail.com>
2018-12-02 08:23:14 -0500

    have to test fitbit

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+function fitbit(){
+    let url;
+    $.get('/authorize').then((res, err) =>{
+        url = res;
+        $('#fitbit').load(url);
+    })
+}
+
 $(function () {
     window.socket = io();
     socket.on('userJoin', function (user) {
@@ -80,5 +88,5 @@ $("#main").on("click", "#startGame", function () {
 $("#usernameForm").submit(function (e) {
     e.preventDefault();
     var userName = $(this).find('input[name="username"]').val();
-    $("#main").html('<p>Great! Now pick one of the two options:</p><div class="row"><div class="col"><button class="btn btn-outline-primary" id="createRoom" data-username="' + userName + '">Create a room</button></div><div class="col"><button class="btn btn-outline-primary" id="joinRoom" data-username="' + userName + '">Join a room</button></div>');
+    $("#main").html('<p>Great! Now pick one of the two options:</p><div class="row"><div class="col"><button class="btn btn-outline-primary" id="createRoom" data-username="' + userName + '">Create a room</button></div><div class="col"><button class="btn btn-outline-primary" id="joinRoom" data-username="' + userName + '">Join a room</button><button class="btn btn-outline-primary" onclick="fitbit()">Link Fitbit</button><div id="fitbit"></div></div>');
 })
