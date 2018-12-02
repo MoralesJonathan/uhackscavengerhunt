fd402833abb4f98fac2cb10adee6b1c742f76358
Fulton Garcia <fultongarcia@gmail.com>
2018-12-02 02:19:34 -0500

    fitbit routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express'),
     app = express(),
     http = require('http').Server(app),
@@ -7,8 +8,13 @@ const express = require('express'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     assert = require('assert'),
-    MongoClient = require('mongodb').MongoClient;
-
+    MongoClient = require('mongodb').MongoClient,
+    FitbitApiClient = require("fitbit-node"),
+    client = new FitbitApiClient({
+        clientId: process.env.FITBIT_CLIENT_ID,
+        clientSecret: process.env.FITBIT_CLIENT_SECRET,
+        apiVersion: '1.2' // 1.2 is the default
+    });
 app
     .use(express.static('public'))
     .use(bodyParser.json())
@@ -18,8 +24,25 @@ app
     .use(logger(environment == 'development' ? 'dev' : 'combined'))
 
     .get("/", (req, res) => res.sendFile('/index.html'))
-    
+    .get("/authorize", (req, res) => {
+        // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
+        res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'YOUR_CALLBACK_URL'));
+    })
+    .get("/callback", (req, res) => {
+        // exchange the authorization code we just received for an access token
+        client.getAccessToken(req.query.code, 'YOUR_CALLBACK_URL').then(result => {
+            // use the access token to fetch the user's profile information
+            client.get("/profile.json", result.access_token).then(results => {
+                res.send(results[0]);
+            }).catch(err => {
+                res.status(err.status).send(err);
+            });
+        }).catch(err => {
+            res.status(err.status).send(err);
+        });
+    });
 
+    
 io.on('connection', function (socket) {
 });
 
