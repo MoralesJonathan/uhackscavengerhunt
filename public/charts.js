14632d530b3d8489bcba7ea2c043ceb1b3ebbcf3
Fulton Garcia <fultongarcia@gmail.com>
2018-12-02 12:03:35 -0500

    added last chart

--- public/charts.js
+++ public/charts.js
@@ -2,11 +2,25 @@
 $.post('/data', {access_token: localStorage.getItem("access_token"), date: "2018-02-15"}).then(res => {
     console.log(res);
     var ctx = document.getElementById("steps");
+    var aChart = document.getElementById("steps");
     let steps = res[0]["activities-steps"][0].value;
     let activities = res[1];
     let activitiesLabels = [];
+    let activitiesDatasets = [{}]
+    activitiesDatasets[0].borderColor = ['rgba(255,99,132,1)',
+    'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 0.2)']
+    activitiesDatasets[0].borderWidth = 1;
+    var activitiesChart = new Chart(aChart, {
+        type: 'doughnut',
+        data: {
+            labels: activitiesLabels,
+            datasets: activitiesDatasets
+        }
+    })
+    
     activities.forEach(activity => {
         activitiesLabels.push(activity.name);
+        activitiesDatasets[0].data.push(activity.distance);
     });
     var stepsChart = new Chart(ctx, {
         type: 'doughnut',
