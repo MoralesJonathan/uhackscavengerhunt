81f66aa9c26e83273485db341ed47e023ada67f2
Jonathan Morales <jonmorazav@yahoo.com>
2018-12-02 04:17:43 -0500

    working room start/stop

--- public/index.js
+++ public/index.js
@@ -1,15 +1,61 @@
 $(function () {
     window.socket = io();
+    socket.on('userJoin', function (user) {
+        var playerCount = parseInt($('#playerCount').text());
+        $('#playerCount').text(playerCount + 1)
+    });
+    socket.on('gameStart', function (user) {
+        $("#main").html("<video autoplay='true' id='cameraFeed'> </video>");
+        var videoFeed = $("#cameraFeed")[0];
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function (stream) {
+                    videoFeed.srcObject = stream;
+                })
+                .catch(function (error) {
+                    console.log("Something went wrong! " + error);
+                });
+        }
+    }); 
+    socket.on('closeRoom', function (user) {
+        alert("Host has closed the connection. Going back to main menu...")
+        location.reload();
+    });
 });
-$("#main").on("click","#createRoom", function(){
-    alert("CREATING ROOM")
+$("#main").on("click", "#createRoom", function () {
+    socket.emit('createRoom', $(this).attr('data-username'), function (roomCode) {
+        if (roomCode !== null) {
+            $("#main").html("<h1>Your room code is " + roomCode + "</h1><p><span id='playerCount'>1</span> Players connected.</p><button id='startGame' data-roomCode='" + roomCode + "'> Start Game </button>");
+        } else {
+            alert("Erorr creating room please refresh the page and try again.")
+        }
+    });
 })
-$("#main").on("click","#joinRoom", function(){
-    alert("JOINING ROOM")
+$("#main").on("click", "#joinRoom", function () {
+    var code = prompt("Please enter your room code:");
+    socket.emit('joinRoom', $(this).attr('data-username'), code, function (roomCode) {
+        if (roomCode !== null && roomCode !== false) {
+            $("#main").html("<h1>Joined room code " + roomCode + "</h1><p><span id='playerCount'>0</span> Players connected.</p><p>Waiting for game to start...</p>");
+        } else if (user == false) {
+            alert("Sorry that room is full or in session! Try again later.")
+        } else {
+            alert("Error joining game. Try again later.")
+        }
+
+    });
 })
-$("#usernameForm").submit(function(e){
+$("#main").on("click", "#startGame", function () {
+    socket.emit('startGame', $(this).attr('data-roomCode'), function (result) {
+        if (result !== null) {
+            $("#main").html('Realtime dashboard goes here');
+        } else {
+            alert("There was an error starting the game. Please feel free to cry now.")
+        }
+    });
+});
+
+$("#usernameForm").submit(function (e) {
     e.preventDefault();
     var userName = $(this).find('input[name="username"]').val();
-    console.log(userName)
-    $("#main").html('<button id="createRoom">Create</button><button id="joinRoom">Join</button>');
+    $("#main").html('<button id="createRoom" data-username="' + userName + '">Create</button><button id="joinRoom" data-username="' + userName + '">Join</button>');
 })
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express'),
     app = express(),
     http = require('http').Server(app),
@@ -7,8 +8,13 @@ const express = require('express'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     assert = require('assert'),
-    MongoClient = require('mongodb').MongoClient;
-
+    MongoClient = require('mongodb').MongoClient,
+    FitbitApiClient = require("fitbit-node"),
+    client = new FitbitApiClient({
+        clientId: process.env.FITBIT_CLIENT_ID,
+        clientSecret: process.env.FITBIT_CLIENT_SECRET,
+        apiVersion: '1.2' // 1.2 is the default
+    });
 app
     .use(express.static('public'))
     .use(bodyParser.json())
@@ -18,9 +24,127 @@ app
     .use(logger(environment == 'development' ? 'dev' : 'combined'))
 
     .get("/", (req, res) => res.sendFile('/index.html'))
-    
+    .get("/authorize", (req, res) => {
+        // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
+        res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'https://secret-springs-39445.herokuapp.com/callback'));
+    })
+    .get("/callback", (req, res) => {
+        // exchange the authorization code we just received for an access token
+        client.getAccessToken(req.query.code, 'https://secret-springs-39445.herokuapp.com/callback').then(result => {
+            // use the access token to fetch the user's profile information
+            client.get("/profile.json", result.access_token).then(results => {
+                res.send(results[0]);
+            }).catch(err => {
+                res.status(err.status).send(err);
+            });
+        }).catch(err => {
+            res.status(err.status).send(err);
+        });
+    });
 
+    
 io.on('connection', function (socket) {
+    socket.on('createRoom', function (user, cb) {
+        const roomName = Math.random().toString(36).substring(7);
+        socket.join(roomName, () => {
+            (async function () {
+                const url = process.env.MONGODB_URI;
+                const client = new MongoClient(url, { useNewUrlParser: true });
+                try {
+                    await client.connect();
+                    console.log("Connected to MLab server");
+                    const db = client.db('heroku_wnkn62l1');
+                    const collection = db.collection('games');
+                    collection.insertOne({ 'room': roomName, 'socket': socket.id, 'username': user, 'active': false }, function (err, result) {
+                        assert.equal(err, null);
+                        cb(roomName)
+                    });
+                } catch (err) {
+                    console.log(err.stack);
+                    cb(null)
+                }
+                client.close();
+            })();
+        });
+    });
+    socket.on('joinRoom', function (user, code, cb) {
+        (async function () {
+            const url = process.env.MONGODB_URI;
+            const client = new MongoClient(url, { useNewUrlParser: true });
+            try {
+                await client.connect();
+                console.log("Connected to MLab server");
+                const db = client.db('heroku_wnkn62l1');
+                const collection = db.collection('games');
+                collection.findOne({ 'room': code }, function (err, game) {
+                    assert.equal(err, null);
+                    !game.active ? (
+                        socket.join(code, () => {
+                            cb(code);
+                            io.sockets.in(code).emit('userJoin', user);
+                        })
+                    ) : cb(false);;
+                });
+            } catch (err) {
+                console.log(err.stack);
+                cb(null)
+            }
+            client.close();
+        })();
+    });
+    socket.on('startGame', function (code, cb) {
+        (async function () {
+            const url = process.env.MONGODB_URI;
+            const client = new MongoClient(url, { useNewUrlParser: true });
+            try {
+                await client.connect();
+                console.log("Connected to MLab server");
+                const db = client.db('heroku_wnkn62l1');
+                const collection = db.collection('games');
+                collection.updateOne({ 'room': code }, { $set: { 'active': true } }, function (err, result) {
+                    assert.equal(err, null);
+                    cb(true)
+                    socket.broadcast.to(code).emit('gameStart');
+                });
+            } catch (err) {
+                console.log(err.stack);
+                cb(null)
+            }
+            client.close();
+        })();
+    });
+    socket.on('disconnect', function () {
+        (async function () {
+            const url = process.env.MONGODB_URI;
+            const client = new MongoClient(url, { useNewUrlParser: true });
+            try {
+                await client.connect();
+                console.log("Connected to MLab server");
+                const db = client.db('heroku_wnkn62l1');
+                const collection = db.collection('games');
+                collection.findOne({ 'socket': socket.id }, function (err, game) {
+                    assert.equal(err, null);
+                    if(game != null){
+                    let roomCode = game.room;
+                    collection.deleteOne({'socket': socket.id}, function(err, result) {
+                        assert.equal(err, null);
+                        assert.equal(1, result.result.n);
+                        io.sockets.in(roomCode).emit('closeRoom');
+                        client.close();
+                      });
+                    }
+                });
+                } catch (err) {
+                console.log(err.stack);
+                cb(null)
+                client.close();
+            }
+        })();
+        io.emit('user disconnected');
+      });
 });
 
 http.listen(port, () => console.log(`Server is running on port ${port} and environment ${environment}`));
+
+
+
